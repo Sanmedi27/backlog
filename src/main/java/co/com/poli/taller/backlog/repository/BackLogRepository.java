f49af6922de4950f47d6be6782dc0db7158ebc63
SantiagoMedinaNRA <santiago.medina@numrot.com>
2020-11-25 20:53:27 -0500

    Se agrega con config server

--- src/main/java/co/com/poli/taller/backlog/repository/BackLogRepository.java
+++ src/main/java/co/com/poli/taller/backlog/repository/BackLogRepository.java
@@ -1,8 +1,9 @@
 package co.com.poli.taller.backlog.repository;
+
 import co.com.poli.taller.backlog.domain.BackLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BackLogRepository extends JpaRepository<BackLog, Long>{
+public interface BackLogRepository extends JpaRepository<BackLog, Long> {
 }
--- src/main/java/co/com/poli/taller/backlog/service/BackLogService.java
+++ src/main/java/co/com/poli/taller/backlog/service/BackLogService.java
@@ -1,9 +1,13 @@
 package co.com.poli.taller.backlog.service;
+
 import co.com.poli.taller.backlog.domain.BackLog;
 
 import java.util.List;
+
 public interface BackLogService {
     List<BackLog> getAllBackLogs();
+
     BackLog getBackLog(Long id);
+
     BackLog createBackLog(BackLog backLog);
 }

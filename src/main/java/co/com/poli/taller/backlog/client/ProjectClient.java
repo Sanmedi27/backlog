3d1b71b127502adf577862a9631e5ddab8f78ab4
SantiagoMedinaNRA <santiago.medina@numrot.com>
2020-12-05 15:20:20 -0500

    Se arregla y se configura.

--- src/main/java/co/com/poli/taller/backlog/client/ProjectClient.java
+++ src/main/java/co/com/poli/taller/backlog/client/ProjectClient.java
@@ -3,11 +3,11 @@ package co.com.poli.taller.backlog.client;
 import co.com.poli.taller.backlog.model.Project;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "service-project")
-@RequestMapping(value = "/project")
+@FeignClient(name = "project-client")
 public interface ProjectClient {
-    public ResponseEntity<Project> getProjectByIdentifier(@PathVariable("projectIdentifier") String projectIdentifier) ;
+    @GetMapping(value = "/{project_identifier}")
+    ResponseEntity<Project> getProjectByIdentifier(@PathVariable("project_identifier") String projectIdentifier) ;
 }
--- src/main/java/co/com/poli/taller/backlog/client/ProjectTaskClient.java
+++ src/main/java/co/com/poli/taller/backlog/client/ProjectTaskClient.java
@@ -3,13 +3,14 @@ package co.com.poli.taller.backlog.client;
 import co.com.poli.taller.backlog.model.ProjectTask;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-@FeignClient(name = "service-projecttask")
+@FeignClient(name = "projecttask-service")
 @RequestMapping(value = "/task")
 public interface ProjectTaskClient {
-
-    public ResponseEntity<List<ProjectTask>> listAllProjectTask();
+    @GetMapping
+    ResponseEntity<List<ProjectTask>> listAllProjectTask();
 }
